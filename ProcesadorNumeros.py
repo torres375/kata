13260b81150746ba26ef4213ca62c9e93f5b4465
torres <31191475+torres375@users.noreply.github.com>
2017-10-01 16:08:23 -0500

    Paso 2: se hace para que funcione el calculo de numero mínimo con una cadena de 1 numero (verde)

--- ProcesadorNumeros.py
+++ ProcesadorNumeros.py
@@ -4,7 +4,13 @@ class ProcesadorNumeros:
             cantidad=0
         elif ("," in cadena):
             cantidad=len(cadena.split(","))
-        else: 
-            cantidad=1
-        minimo = 0
+        else:
+            cantidad = 1
+        if (cadena==""):
+            minimo = 0
+        elif(len(cadena)==1):
+            minimo= int(cadena)
+        else:
+            minimo= 0
         return {cantidad, minimo, 0, 0}
+
--- ProcesadorNumerosTest.py
+++ ProcesadorNumerosTest.py
@@ -3,13 +3,14 @@ from unittest import TestCase
 from ProcesadorNumeros import ProcesadorNumeros
 
 
+
 class ProcesadorNumerosTest(TestCase):
     def test_procesarCantidad(self):
         result = {0, 0, 0, 0}
         self.assertEqual(ProcesadorNumeros().procesar(""), result, "Cadena vacia");
 
     def test_procesar1NumeroCantidad(self):
-        result = {1, 0, 0, 0}
+        result = {1, 4, 0, 0}
         self.assertEqual(ProcesadorNumeros().procesar("4"), result, "Cadena Con Un Numero");
 
     def test_procesarCadenaDosNumerosCantidad(self):
@@ -19,13 +20,4 @@ class ProcesadorNumerosTest(TestCase):
     def test_procesarCadenaconNNumerosCantidad(self):
         result = {5, 0, 0, 0}
         self.assertEqual(ProcesadorNumeros().procesar("2,4,5,6,8"), result, "Cadena Con N Numeros");
-
-    def test_procesarMinimo(self):
-        result = {0, 0, 0, 0}
-        self.assertEqual(ProcesadorNumeros().procesar(""), result, "Cadena vacia");
-        
-        
-    def test_procesarMinimo1numero(self):
-        result = {1, 4, 0, 0}
-        self.assertEqual(ProcesadorNumeros().procesar("4"), result, "Cadena Con Un Numero");
 
