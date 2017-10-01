8c4e8c6978f64ae3ba6deb1b8acb3d51867d6ada
Oscar Amaya <og.amaya@uniandes.edu.co>
2017-10-01 15:16:00 -0500

    Paso 1: Devolver un arreglo con el número de elementos, se mejora el método para que funcione con cadena de dos numeros (Verde)

--- ProcesadorNumeros.py
+++ ProcesadorNumeros.py
@@ -2,6 +2,8 @@ class ProcesadorNumeros:
     def procesar(self, cadena):
         if (cadena==""):
             cantidad=0
+        elif ("," in cadena):
+            cantidad=2
         else: 
             cantidad=1
         return {cantidad, 0, 0, 0}
