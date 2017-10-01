bd5c9af93b579fa81a66a0b1573b07568794a11c
torres <31191475+torres375@users.noreply.github.com>
2017-10-01 14:57:09 -0500

    Paso1
    Se agrega test para que funcione con un string de 1 numero (rojo)

--- ProcesadorNumerosTest.py
+++ ProcesadorNumerosTest.py
@@ -7,3 +7,7 @@ class ProcesadorNumerosTest(TestCase):
     def test_procesar(self):
         result = {0, 0, 0, 0}
         self.assertEqual(ProcesadorNumeros().procesar(""), result, "Cadena vacia");
+    
+    def test_procesar1Numero(self):
+        result = {1, 0, 0, 0}
+        self.assertEqual(ProcesadorNumeros().procesar("4"), result, "Cadena Con Un Numero");
