8ac56f2e6e6097e8f288cc46e5d75cf711799d75
torres <31191475+torres375@users.noreply.github.com>
2017-10-01 16:52:16 -0500

    Paso 2: se modifican los valores para que funcione con un string de 1 numero (verde)

--- ProcesadorNumeros.py
+++ ProcesadorNumeros.py
@@ -6,16 +6,15 @@ class ProcesadorNumeros:
             cantidad=len(cadena.split(","))
         else:
             cantidad = 1
-
+        maximo = 0;
         minimo = 0
         if(len(cadena)==1):
             minimo= int(cadena)
+            maximo= int(cadena)
         elif(len(cadena)>1):
             numeros = cadena.split(",")
             for n in numeros:
                 if (minimo > int(n)):
                     minimo = int(n)
-        maximo = -1;
+
         return {cantidad, minimo, maximo, 0}
-
-
