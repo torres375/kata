70331fb008f7ae571ef63f2089d7c6b81bbe689f
Oscar Amaya <31192501+OGamaya@users.noreply.github.com>
2017-10-01 16:18:47 -0500

    Paso2: Se modifica el test para que falle con dos numeros (rojo)

--- ProcesadorNumerosTest.py
+++ ProcesadorNumerosTest.py
@@ -12,10 +12,10 @@ class ProcesadorNumerosTest(TestCase):
     def test_procesar1NumeroCantidad(self):
         result = {1, 4, 0, 0}
         self.assertEqual(ProcesadorNumeros().procesar("4"), result, "Cadena Con Un Numero");
-
+    
     def test_procesarCadenaDosNumerosCantidad(self):
-        result = {2, 0, 0, 0}
-        self.assertEqual(ProcesadorNumeros().procesar("2,4"), result, "Cadena Con dos Numeros");
+        result = {2, -1, 0, 0}
+        self.assertEqual(ProcesadorNumeros().procesar("2,-1"), result, "Cadena Con dos Numeros");
 
     def test_procesarCadenaconNNumerosCantidad(self):
         result = {5, 0, 0, 0}
